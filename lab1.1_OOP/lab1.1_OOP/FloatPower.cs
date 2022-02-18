24ee3ca26df5e24a6fc9f157e879b444146ba48b
VladyslavStashkevych <vladyslav.stashkevych.it.2021@lpnu.ua>
2022-02-18 16:18:45 +0200

    lab completed

---
+++ lab1.1_OOP/lab1.1_OOP/FloatPower.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace lab1._1_OOP {
+    class FloatPower {
+        double first;
+        double second;
+
+        public double First {
+            get => first;
+            set => first = value;
+            
+        }
+        public double Second {
+            get => second;
+            set => second = value;
+        }
+
+        public bool Init(double x, double y) {
+            if (x != 0) {
+                this.First = x;
+                this.Second = y;
+                return true;
+            }
+            Console.WriteLine("Number should not be zero!");
+            return false;
+        }
+        public void Read() {
+            Console.Write("Enter number: ");
+            double num = Double.Parse(Console.ReadLine());
+
+            Console.Write("Enter power: ");
+            double power = Double.Parse(Console.ReadLine());
+            if (!this.Init(num, power))
+                do {
+                    Console.Write("Enter another number: ");
+                    num = Double.Parse(Console.ReadLine());
+                } while (!this.Init(num, power));
+        }
+        public void Display() {
+            var pow = this.Power();
+            if (pow % 1 == 0)
+                Console.WriteLine($"Number {this.First} raised to {this.Second} equals {pow}");
+            else
+                Console.WriteLine($"Number {this.First} raised to {this.Second} equals {pow:0.0000}");
+        }
+        public double Power() => Math.Pow(this.First, this.Second);
+    }
+}
---
+++ lab1.1_OOP/lab1.1_OOP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab1._1_OOP {
+    static class Program {
+        static void Main(string[] args) {
+            FloatPower powerRD = new FloatPower();
+            powerRD.Read();
+            powerRD.Display();
+            Console.WriteLine();
+
+            FloatPower powerID = new FloatPower();
+            powerID.Init(-0.1, 3);
+            powerID.Display();
+            Console.WriteLine();
+
+            FloatPower powerMD = MakeFloatPower(4, 2.5);
+            powerMD.Display();
+            Console.WriteLine();
+        }
+        public static FloatPower MakeFloatPower(double num, double pow) {
+            var result = new FloatPower();
+            result.Init(num, pow);
+            return result;
+        }
+    }
+}
